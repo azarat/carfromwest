44f457699037f5cd2020fd8223561c5fbd904d27
Bohdan .K <spudegbutman@gmail.com>
2021-05-31 15:05:47 +0300

    add: faq

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import AboutUs from '../src/components/HomePage/AboutUs/AboutUs'
 import Request from '../src/components/HomePage/Request/Request'
 import PopularAuto from '../src/components/PopularAuto/PopularAuto'
 import ClientReviews from '../src/components/HomePage/ClientsReview/ClientReviews'
+import Faq from '../src/components/HomePage/Faq/Faq'
 
 const IndexPage: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const IndexPage: NextPage = () => {
       <Request />
       <PopularAuto />
       <ClientReviews />
+      <Faq />
     </>
   )
 }
