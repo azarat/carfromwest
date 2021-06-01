31543c5a58e3b07bea0773ec9acb4ed78c0a2560
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2021-06-01 14:24:41 +0300

    add: form send

--- src/components/HomePage/Request/Request.tsx
+++ src/components/HomePage/Request/Request.tsx
@@ -1,23 +1,26 @@
-import React, { ChangeEvent, useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
+import { Formik } from 'formik'
 import InputMask from 'react-input-mask'
 import SubTitle from '../../SubTitle/SubTitle'
 import Title from '../../Title/Title'
 
+const numberRegEpx = /^\+380\(\d{2}\) \d{3}-\d{2}-\d{2}$/
+
 const Request: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string>('')
-  const [phoneValue, setPhoneValue] = useState<string>('')
-  const [textAreaValue, setTextAreaValue] = useState<string>('')
+  const sendRequest = async (values: any) => {
+    const res = await fetch(
+      'https://admin.webrains.studio/sendCFWLandingMessage',
+      {
+        method: 'POST',
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-  }
-  const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPhoneValue(e.target.value)
-  }
-
-  const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setTextAreaValue(e.target.value)
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      }
+    )
+    console.log(res)
   }
 
   return (
@@ -29,52 +32,80 @@ const Request: React.FC = () => {
           Заполните форму, чтобы мы связались с Вами и сказали какой автомобиль
           Вы можете себе пригнать из США, сэкономив до 40% его стоимости.
         </div>
-        <form className="request__info-form">
-          <div className="request__info-form-input--wrapper">
-            <input
-              id="request-name"
-              type="text"
-              className="request__info-form-input"
-              value={inputValue}
-              onChange={handleInputChange}
-            />
-            {inputValue.length === 0 && (
-              <label htmlFor="request-name">Введите ваше имя</label>
-            )}
-            {inputValue.length === 0 && (
-              <p className="request__info-form-input-error">
-                Обязательное поле
-              </p>
-            )}
-          </div>
-          <div className="request__info-form-input--wrapper">
-            <InputMask
-              value={phoneValue}
-              onChange={handlePhoneChange}
-              className="request__info-form-input"
-              mask="+380\(99) 999-99-99"
-              alwaysShowMask
-            />
-            {phoneValue.length === 0 && (
-              <p className="request__info-form-input-error">
-                Обязательное поле
-              </p>
-            )}
-          </div>
-          <div className="request__info-form-input--wrapper request__info-form-textarea--wrapper">
-            <textarea
-              id="request-text"
-              className="request__info-form-textarea"
-              value={textAreaValue}
-              onChange={handleTextAreaChange}
-            />
-            <label htmlFor="request-text">Введите ваши пожелания</label>
-          </div>
 
-          <button className="request__info-form-button" type="submit">
-            Подобрать лучшее авто
-          </button>
-        </form>
+        <Formik
+          initialValues={{ name: '', phone: '', wishes: '' }}
+          validate={(values) => {
+            const errors: any = {}
+            if (!values.name) {
+              errors.name = 'Обязательное поле'
+            }
+            if (!values.phone) {
+              errors.phone = 'Обязательное поле'
+            } else if (!numberRegEpx.test(values.phone)) {
+              errors.phone = 'Не правильно введен номер'
+            }
+            return errors
+          }}
+          onSubmit={(values, { resetForm }) => {
+            sendRequest(values)
+            resetForm({})
+          }}
+        >
+          {({ values, touched, handleSubmit, handleChange, errors }) => (
+            <form onSubmit={handleSubmit} className="request__info-form">
+              <div className="request__info-form-input--wrapper">
+                <input
+                  name="name"
+                  id="request-name"
+                  type="text"
+                  className="request__info-form-input"
+                  value={values.name}
+                  onChange={handleChange}
+                />
+                {values.name.length === 0 && (
+                  <label htmlFor="request-name">Введите ваше имя</label>
+                )}
+                {errors.name && touched.name && (
+                  <p className="request__info-form-input-error">
+                    {errors.name}
+                  </p>
+                )}
+              </div>
+              <div className="request__info-form-input--wrapper">
+                <InputMask
+                  name="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  className="request__info-form-input"
+                  mask="+380\(99) 999-99-99"
+                  alwaysShowMask
+                />
+                {errors.phone && touched.phone && (
+                  <p className="request__info-form-input-error">
+                    {errors.phone}
+                  </p>
+                )}
+              </div>
+              <div className="request__info-form-input--wrapper request__info-form-textarea--wrapper">
+                <textarea
+                  name="wishes"
+                  id="request-text"
+                  className="request__info-form-textarea"
+                  value={values.wishes}
+                  onChange={handleChange}
+                />
+                {values.wishes.length === 0 && (
+                  <label htmlFor="request-text">Введите ваши пожелания</label>
+                )}
+              </div>
+
+              <button className="request__info-form-button" type="submit">
+                Подобрать лучшее авто
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
       <div className="request__image">
         <h4 className="request__image-title">CARSFROMWEST</h4>
