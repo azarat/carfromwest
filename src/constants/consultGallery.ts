5d03d1c11438a54286bb5aa347eb8fcb599f3022
Bohdan .K <spudegbutman@gmail.com>
2021-06-08 17:53:27 +0300

    fix: img url

--- src/constants/consultGallery.ts
+++ src/constants/consultGallery.ts
@@ -1,46 +1,46 @@
 export const consultGallery = [
   {
     id: 1,
-    url: '/assets/images/consult/consult1.JPG',
+    url: '/assets/images/consult/consult1.jpg',
   },
   {
     id: 2,
-    url: '/assets/images/consult/consult2.JPG',
+    url: '/assets/images/consult/consult2.jpg',
   },
   {
     id: 3,
-    url: '/assets/images/consult/consult3.JPG',
+    url: '/assets/images/consult/consult3.jpg',
   },
   {
     id: 4,
-    url: '/assets/images/consult/consult4.JPG',
+    url: '/assets/images/consult/consult4.jpg',
   },
   {
     id: 5,
-    url: '/assets/images/consult/consult5.JPG',
+    url: '/assets/images/consult/consult5.jpg',
   },
   {
     id: 6,
-    url: '/assets/images/consult/consult6.JPG',
+    url: '/assets/images/consult/consult6.jpg',
   },
   {
     id: 7,
-    url: '/assets/images/consult/consult7.JPG',
+    url: '/assets/images/consult/consult7.jpg',
   },
   {
     id: 8,
-    url: '/assets/images/consult/consult8.JPG',
+    url: '/assets/images/consult/consult8.jpg',
   },
   {
     id: 9,
-    url: '/assets/images/consult/consult9.JPG',
+    url: '/assets/images/consult/consult9.jpg',
   },
   {
     id: 10,
-    url: '/assets/images/consult/consult10.JPG',
+    url: '/assets/images/consult/consult10.jpg',
   },
   {
     id: 11,
-    url: '/assets/images/consult/consult11.JPG',
+    url: '/assets/images/consult/consult11.jpg',
   },
 ]
