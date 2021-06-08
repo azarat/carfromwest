1de92581bdfbbbc40e11789b046f734f656cbc44
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2021-06-08 13:40:29 +0300

    add: catalog link

--- src/constants/NavItems.ts
+++ src/constants/NavItems.ts
@@ -1,36 +1,37 @@
 export const NavItems = [
+  { id: 0, title: 'Каталог', href: '/catalog' },
   {
-    id: 0,
+    id: 1,
     title: 'О нас',
     href: '#section-about',
   },
   {
-    id: 1,
+    id: 2,
     title: 'Популярные авто',
     href: '#section-popular',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Отзывы',
     href: '#client-reviews-section',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Преимущества',
     href: '#advantages-section',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Процесс доставки',
     href: '#delivery-section',
   },
   {
-    id: 5,
+    id: 6,
     title: 'FAQ',
     href: '#section-faq',
   },
   {
-    id: 6,
+    id: 7,
     title: 'О сервисе',
     href: '#',
   },
