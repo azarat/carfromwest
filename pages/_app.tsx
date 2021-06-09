d3358441e15b413ef8e487bc27972930c71ae009
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2021-06-09 10:02:20 +0300

    add: title and binotel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/main.scss'
 import Header from '../src/components/Header/Header'
 import Footer from '../src/components/Footer/Footer'
@@ -7,6 +8,25 @@ import NextNprogress from 'nextjs-progressbar'
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <title>
+          Авто из США с Аукционов Копарт, IAAI, Манхейм | CarsFromWest
+        </title>
+        {/* Binotel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, w, s) {
+              var widgetHash = '4v5d5f2mkx39hckkovwn', gcw = d.createElement(s); gcw.type = 'text/javascript';gcw.async = true;
+              gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+              var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+              })(document, window, 'script');
+
+          `,
+          }}
+          type="text/javascript"
+        />
+      </Head>
       <Header />
       <main>
         <Component {...pageProps} />
@@ -23,6 +43,5 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 }
 
 export default MyApp
-
 
 // TODO: COOMMENT FOR DEPLOY
