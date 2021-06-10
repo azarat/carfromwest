3a9961dd7809b003c823e912328b080213be6970
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2021-06-10 12:17:17 +0300

    fix: filter

--- src/constants/NavItems.ts
+++ src/constants/NavItems.ts
@@ -1,38 +1,38 @@
 export const NavItems = [
-  { id: 0, title: 'Каталог', href: '/catalog' },
   {
-    id: 1,
+    id: 0,
+    title: 'Главная',
+    href: '/',
+  },
+  { id: 1, title: 'Каталог', href: '/catalog' },
+  {
+    id: 2,
     title: 'О нас',
     href: '/#section-about',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Популярные авто',
     href: '/#section-popular',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Отзывы',
     href: '/#client-reviews-section',
   },
   {
-    id: 4,
+    id: 5,
     title: 'Преимущества',
     href: '/#advantages-section',
   },
   {
-    id: 5,
+    id: 6,
     title: 'Процесс доставки',
     href: '/#delivery-section',
   },
   {
-    id: 6,
+    id: 7,
     title: 'FAQ',
     href: '/#section-faq',
-  },
-  {
-    id: 7,
-    title: 'О сервисе',
-    href: '/#',
   },
 ]
