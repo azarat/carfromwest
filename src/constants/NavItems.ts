8eaa8e0cd8d0231a2cd28cc99f4523fd3663d752
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2021-06-10 11:58:35 +0300

    fix: nav items from catalog

--- src/constants/NavItems.ts
+++ src/constants/NavItems.ts
@@ -3,36 +3,36 @@ export const NavItems = [
   {
     id: 1,
     title: 'О нас',
-    href: '#section-about',
+    href: '/#section-about',
   },
   {
     id: 2,
     title: 'Популярные авто',
-    href: '#section-popular',
+    href: '/#section-popular',
   },
   {
     id: 3,
     title: 'Отзывы',
-    href: '#client-reviews-section',
+    href: '/#client-reviews-section',
   },
   {
     id: 4,
     title: 'Преимущества',
-    href: '#advantages-section',
+    href: '/#advantages-section',
   },
   {
     id: 5,
     title: 'Процесс доставки',
-    href: '#delivery-section',
+    href: '/#delivery-section',
   },
   {
     id: 6,
     title: 'FAQ',
-    href: '#section-faq',
+    href: '/#section-faq',
   },
   {
     id: 7,
     title: 'О сервисе',
-    href: '#',
+    href: '/#',
   },
 ]
