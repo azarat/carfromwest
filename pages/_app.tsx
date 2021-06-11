c04672c32fc312c34579bd4ba6e320638cc527f4
Bohdan .K <spudegbutman@gmail.com>
2021-06-11 16:42:20 +0300

    add: smartlook code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,18 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           }}
           type="text/javascript"
         />
+        {/*SmartLook Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', '8a4fe5c68e84b37e58c4b8729df3ed278a4ad30a');`,
+          }}
+          type="text/javascript"
+        ></script>
       </Head>
       <Header />
       <main>
