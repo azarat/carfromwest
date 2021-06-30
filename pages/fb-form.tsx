023d5def3cd9796e5ca758ffd0e1868cc9b98b83
Bohdan .K <spudegbutman@gmail.com>
2021-06-30 13:42:48 +0300

    add: fb consult form

---
+++ pages/fb-form.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+import React from 'react'
+import ConsultForm from '../src/components/ConsultForm/ConsultForm'
+
+const FbForm: NextPage = () => {
+  return <ConsultForm />
+}
+
+export default FbForm
---
+++ src/components/ConsultForm/ConsultForm.tsx
@@ -0,0 +1,66 @@
+import { useFormik } from 'formik'
+import React from 'react'
+import InputMask from 'react-input-mask'
+import * as Yup from 'yup'
+
+const fbValidationSchema = {
+  phone: Yup.string().min(13).required('Поле обязательное'),
+}
+
+const ConsultForm: React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      phone: '',
+    },
+    validationSchema: Yup.object().shape(fbValidationSchema),
+    onSubmit: (values) => {
+      fetch(`https://admin.webrains.studio/sendCFWLandingMessage`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: values.name, phone: values.phone }),
+      }).then(
+        (res) =>
+          res.status === 200 &&
+          typeof window !== 'undefined' &&
+          window.location.assign('/thanks')
+      )
+    },
+  })
+  return (
+    <div className="consult-form-wrapper">
+      <div className="consult-form">
+        <h1 className="consult-form-title">
+          Заполните форму, чтобы записаться на аудит
+        </h1>
+        <input
+          className="consult-form-input"
+          type="text"
+          name="name"
+          placeholder="Имя"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <InputMask
+          alwaysShowMask
+          maskChar=""
+          className={`consult-form-input ${
+            formik.errors.phone ? 'consult-form-input-error' : ''
+          }`}
+          type="text"
+          name="phone"
+          value={formik.values.phone}
+          mask="+380999999999"
+          onChange={formik.handleChange}
+        ></InputMask>
+        <button className="consult-form-button" onClick={formik.submitForm}>
+          ХОЧУ АУДИТ
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ConsultForm
