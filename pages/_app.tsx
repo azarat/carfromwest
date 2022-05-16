96d8acc67d2b48a8ab01cd5222fa6dd1455c0121
Karyna Mazur <50112425+KarynaMazur@users.noreply.github.com>
2022-05-16 11:30:47 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,22 +43,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-MHFMKJC');`}}>
         </script>
         {/* HelpCrunch chat Code */}
-        <script type="text/javascript">
-(function(w,d){
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d){
   w.HelpCrunch=function(){w.HelpCrunch.q.push(arguments)};w.HelpCrunch.q=[];
   function r(){var s=document.createElement('script');s.async=1;s.type='text/javascript';s.src='https://widget.helpcrunch.com/';(d.body||d.head).appendChild(s);}
   if(w.attachEvent){w.attachEvent('onload',r)}else{w.addEventListener('load',r,false)}
-})(window, document)
-</script>
-
-<script type="text/javascript">
-  HelpCrunch('init', 'carsfromwest', {
+})(window, document)`}}>
+        </script>
+        <script dangerouslySetInnerHTML={{
+          __html: `HelpCrunch('init', 'carsfromwest', {
     applicationId: 1,
     applicationSecret: 'KHddEJ19qmyTlPTtq9A5G3oT973HnUc80Bv3vBM6kMU4DZIoWxE5oiyXDdcoYNzNEDofv6KvUf92dYQL75dj4g=='
   })
 
-  HelpCrunch('showChatWidget');
-</script>
+  HelpCrunch('showChatWidget');`}}>
+        </script>
         {/* Binotel Code */}
         <script
           type="text/javascript"
