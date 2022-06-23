9b297d64e6a79eb551b72c35fb737c360172cb4b
VadymDenysiuk <80970410371q@gmail.com>
2022-06-23 19:50:16 +0300

    add: new styles to the section Order

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next'
-import React, { MouseEventHandler, useState } from 'react'
+import React, { MouseEventHandler, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import InputMask from 'react-input-mask'
 import ArrowBackSVG from '../src/assets/svg/arrowBack.svg'
+import Link from 'next/link'
+import RightPolygonSVG from '../src/assets/svg/right-polygon.svg'
 
 const numberRegEpx = /^\+380\(\d{2}\) \d{3}-\d{2}-\d{2}$/
 
@@ -11,8 +13,12 @@ const Order: NextPage = () => {
   const [name, setName] = useState<string>('')
   const [phone, setPhone] = useState<string>('')
   const [errors, setErrors] = useState<string[]>([])
+  const [autoVin, setAutoVin] = useState<any>()
+  const [isFormSend, setIsFormSend] = useState<boolean>(false)
   const router = useRouter()
-
+  useEffect(() => {
+    setAutoVin(localStorage.getItem("autoVin"))
+  }, [autoVin])
   const handleSend: MouseEventHandler = async (e) => {
     const url = localStorage.getItem('url')
     e.preventDefault()
@@ -38,8 +44,9 @@ const Order: NextPage = () => {
       if (res.status === 200) {
         setName('')
         setPhone('')
-        localStorage.removeItem('url')
-        router.push({ pathname: '/thanks' })
+        // localStorage.removeItem('url')
+        // router.push({ pathname: '/thanks' })
+        setIsFormSend(true)
       }
     }
   }
@@ -47,69 +54,121 @@ const Order: NextPage = () => {
   const validate = (): string[] => {
     const initErrors: string[] = []
     if (name.length < 2) initErrors.push('name')
-    if (!numberRegEpx.test(phone)) initErrors.push('phone')
+    // if (!numberRegEpx.test(phone)) initErrors.push('phone')
     setErrors([...initErrors])
     return initErrors
   }
 
   return (
     <div className="order">
+      
       <div className="order__wrapper">
-        <ArrowBackSVG className="order__back" onClick={() => router.back()} />
-        <h1 className="order__title">посчитать стоимость</h1>
-        <form className="order__form">
-          <input
-            type="text"
-            className={`order__form-input ${
-              errors.includes('name') ? 'order__form-input--error' : ''
-            }`}
-            placeholder="Ваше имя"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <InputMask
-            mask="+380\(99) 999-99-99"
-            alwaysShowMask
-            type="text"
-            className={`order__form-input ${
-              errors.includes('phone') ? 'order__form-input--error' : ''
-            }`}
-            placeholder="Ваш телефон"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <div className="order__form-checkbox-wrapper">
-            <input
-              id="tel"
-              name="tel"
-              type="checkbox"
-              className="order__form-checkbox"
-              onChange={() => setConnectType('tel')}
-              checked={connectType === 'tel'}
-            />
-            <label className="order__form-label" htmlFor="tel">
-              Консультация по телефону
-            </label>
+        <div className="containers">
+          <nav className="car-page__breadcrumbs">
+            <ul>
+              <li className="car-page__breadcrumbs-link">
+                <Link href="/">
+                  <a>
+                    Головна <RightPolygonSVG />
+                  </a>
+                </Link>
+              </li>
+              <li className="car-page__breadcrumbs-link">
+                <Link href="/catalog">
+                  <a>
+                    Каталог авто <RightPolygonSVG />
+                  </a>
+                </Link>
+              </li>
+              <li className="car-page__breadcrumbs-link">
+                 <a onClick={() => router.back()}>
+                      {autoVin} <RightPolygonSVG />
+                  </a>
+                   </li>
+                <li className="car-page__breadcrumbs-link">
+                <Link href="/order">
+                  <a>
+                    Форма
+                  </a>
+                </Link></li>
+            </ul>
+          </nav>
           </div>
-          <div className="order__form-checkbox-wrapper">
-            <input
-              type="checkbox"
-              id="messanger"
-              name="messanger"
-              className="order__form-checkbox"
-              onChange={() => setConnectType('messanger')}
-              checked={connectType === 'messanger'}
-            />
-            <label className="order__form-label" htmlFor="messanger">
-              Отправить информацию в Вайбер/Телеграм
-            </label>
-          </div>
-          <button className="order__form-btn" onClick={handleSend}>
-            отправить
+        
+       {isFormSend ? <form className="order__form">
+        <h1 className="order__title-thanks">ДЯКУЄМО
+        ЗА ЗАЯВКУ<span className='order__form-title'>!</span></h1>
+        <p className='order__text'>Наш менеджер зв’яжеться з Вами в найближчий час</p>
+        <button className="order__form-btn" >
+        <Link href="/">
+                  <a>
+                    На головну 
+                  </a>
+                </Link>
           </button>
-        </form>
+      </form>
+        : 
+      <form className="order__form">
+      <h1 className="order__title"><span className='order__form__line-start'></span>ПОРАХУВАТИ ВАРТІСТЬ<span className='order__form__line-end'></span></h1>
+      <div className="order__form__input-wrapper">
+        <span className="order__form__thumb">
+          <input 
+          className={`order__form-input ${
+          errors.includes('name') ? 'order__form-input--error' : ''
+          }`}
+          placeholder="Ваше ім’я"
+          value={name}
+          onChange={(e) => setName(e.target.value)} />
+        </span>
+        <span className="order__form__thumb">
+          <InputMask
+          alwaysShowMask
+          type="text"
+          className={`order__form-input ${
+            errors.includes('phone') ? 'order__form-input--error' : ''
+          }`}
+          placeholder="Номер телефону"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          />
+        </span>
       </div>
-      <div className="order__img"></div>
+      <div className="order__form-checkbox-wrappers">
+        <div className="order__form-checkbox-wrapper">
+          <input
+            id="tel"
+            name="tel"
+            type="checkbox"
+            className="order__form-checkbox"
+            onChange={() => setConnectType('tel')}
+            checked={connectType === 'tel'}
+          />
+          <label className="order__form-label" htmlFor="tel">
+            Консультація по телефону
+          </label>
+        </div>
+        
+        <div className="order__form-checkbox-wrapper">
+          <input
+            type="checkbox"
+            id="messanger"
+            name="messanger"
+            className="order__form-checkbox"
+            onChange={() => setConnectType('messanger')}
+            checked={connectType === 'messanger'}
+          />
+          <label className="order__form-label" htmlFor="messanger">
+            Відправити інформацію в Вайбер/Телеграм
+          </label>
+        </div>
+      </div>
+      <button className="order__form-btn" onClick={handleSend}>
+        Відправити
+      </button>
+    </form> 
+      } 
+      
+      </div>
     </div>
   )
 }
