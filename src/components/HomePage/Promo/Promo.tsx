e13189ec4051e9c4e55e241bbcd12fd4c698b706
VadymDenysiuk <80970410371q@gmail.com>
2022-11-18 17:39:11 +0200

    change main-image to cut version

--- src/components/HomePage/Promo/Promo.tsx
+++ src/components/HomePage/Promo/Promo.tsx
@@ -262,7 +262,7 @@ const Promo: React.FC = (): JSX.Element => {
                 objectFit="cover"
                 layout="fill"
                 loading="eager"
-                src="/assets/images/hero-new.jpg"
+                src="/assets/images/hero-main-bg.jpg"
               />
             </div>
             <div className="promo__image-mob">
